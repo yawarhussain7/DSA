f33f9e6f877f171b42f8897fbafcbf14a756e149
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-23 13:25:19 -0700

    Define comment

--- tutorial_02.java
+++ tutorial_02.java
@@ -8,10 +8,11 @@ class tutorial_02 {
         System.out.print("Hello World");
         System.out.print("Hello world \n with java ");
 
-        //Variable 
+        //Variable              this is comment it will ignore when code run
         String name = "Yawar Hussian";
         int age = 21;
         float weight = 57.3;
         double height = 5.6;
+
     }
 }
