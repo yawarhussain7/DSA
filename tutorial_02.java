ad61089ffd2ae9b364773fd318f229494b140b81
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-23 13:21:20 -0700

    Using escape sequence break the line

--- tutorial_02.java
+++ tutorial_02.java
@@ -1,7 +1,10 @@
 class tutorial_02 {
     
-    public static void main(Stirng args[])
+    public static void main(String args[])
     {
         System.out.println("Hello WOrld");
+
+        System.out.print("Hello World");
+        System.out.print("Hello world \n with java ");
     }
 }
