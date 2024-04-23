b0e74491815eb163eb17f3aa4986f07cd54b23cc
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-23 13:22:24 -0700

    Declear String variable

--- tutorial_02.java
+++ tutorial_02.java
@@ -2,9 +2,13 @@ class tutorial_02 {
     
     public static void main(String args[])
     {
+        //Output
         System.out.println("Hello WOrld");
 
         System.out.print("Hello World");
         System.out.print("Hello world \n with java ");
+
+        //Variable 
+        String name = "Yawar Hussian";
     }
 }
