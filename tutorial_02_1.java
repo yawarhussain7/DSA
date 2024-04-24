dd33b7b39b9536ec8dc4e407a13a58a47dc01355
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-24 11:56:42 -0700

    Declaring basic datatypes

--- tutorial_02_1.java
+++ tutorial_02_1.java
@@ -4,5 +4,12 @@ public class tutorial_02_1 {
     public static void main(String args[])
     {
         Scanner input= new Scanner(System.in);
+        String name;
+        int age;
+        float height;
+        double weight;
+        char fav_letter;
+
+    
     }
 }
