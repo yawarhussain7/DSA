9edf45ac9fc1fc8d01913bb5c3e9c0a9de34ee90
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-24 12:07:38 -0700

    Get values of variable from user and print the sum

--- Question/Tutorial_02.java
+++ Question/Tutorial_02.java
@@ -7,6 +7,13 @@ public class Tutorial_02 {
         Scanner input =new Scanner(System.in);
 
         int num1,num2,sum=0;
-        
+        System.out.print("Enter the value of num1 :> ");
+        num1 = input.nextInt();
+        System.out.print("\nEnter the value of num2 :> ");
+        num2 = input.nextInt();
+
+        sum = num1 + num2;
+
+        System.out.println("SUm = "+sum);
     }
 }
