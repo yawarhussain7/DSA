3ee03f9954f1688f000de3226d58d5114bca0574
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-24 12:05:04 -0700

    Create a directory for Questions which we practice in this course

---
+++ Question/Tutorial_02.java
@@ -0,0 +1,9 @@
+package Question;
+import java.util.Scanner;
+
+// Create two variable and get value from user after that print there sum 
+public class Tutorial_02 {
+    public static void main(String args[]){
+        Scanner input =new Scanner(System.in);
+    }
+}
