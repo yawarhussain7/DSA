ee7d214927d2de0f42bccb65a6978c9c4ee8be06
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-28 13:17:17 -0700

    Switch statement example

---
+++ tutorial_3_2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class tutorial_3_2{
+
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+
+        int week;
+        System.out.print("Enter the number between [ 1 - 7 ] for print week name :> ");
+        week = input.nextInt();
+
+        switch(week)
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+            
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("Invalid input ");
+        }
+    }
+}
