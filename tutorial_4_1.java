cf400f3aae0201dbef02117f224bec9f40035442
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-28 13:38:47 -0700

    While loop in java

---
+++ tutorial_4_1.java
@@ -0,0 +1,12 @@
+class tutorial_4_1{
+    public static void main(String args[])
+    {
+        int counter = 1;
+
+        while(counter <= 10)
+        {
+            System.out.print(counter + " ");
+            counter++;
+        }
+    }
+}
