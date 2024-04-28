8e720707ae3f8bad64765c79d9d118565844a662
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-28 13:46:42 -0700

    find the sum of n term using for loop

---
+++ tutorial_4_3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class tutorial_4_3{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+
+        int range, sum =0 ;
+        System.out.print("Enter the range of number :> ");
+        range = input.nextInt();
+
+        for(int i =1; i <= range;i++)
+        {
+            sum = sum + i;
+            System.out.print(i+" ");
+        }
+        System.out.println("\nSum :> "+sum);
+    }
+}
