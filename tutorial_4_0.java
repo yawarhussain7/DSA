744fb99d20ebcc1d30c138ad5084aa74f6f7d580
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-28 13:26:43 -0700

    for loop intro

---
+++ tutorial_4_0.java
@@ -0,0 +1,9 @@
+class tutorial_4_0{
+    public static void main(String args[])
+    {
+        for(int counter = 0; counter < 3 ;counter++)
+        {
+            System.out.print("Hello world\n");
+        }
+    }
+}
