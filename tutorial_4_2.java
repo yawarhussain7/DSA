580e9d5cdef93bde4e9f6d0ad5195daaeab2cef4
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-28 13:40:23 -0700

    Do while loop in java

---
+++ tutorial_4_2.java
@@ -0,0 +1,10 @@
+class tutorial_4_2{
+    public static void main(String args[])
+    {
+        int number = 1;
+        do{
+            System.out.print(number+" ");
+            number = number +1;
+        }while(number <= 10);
+    }
+}
