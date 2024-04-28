70fda56f2dabff7adbf49d541db3ebfd1f07ae50
Yawar Hussain <yawarhussain793@gmail.com>
2024-04-28 13:54:39 -0700

    Solid Rectangle pattern

---
+++ Solid_Rectangle.java
@@ -0,0 +1,16 @@
+public class Solid_Rectangle {
+
+    public static void main(String args[])
+    {
+        for(int i = 0 ; i <= 5; i++)
+        {
+            for(int j = 0 ; j <= 5;  j++)
+            {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
