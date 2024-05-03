3aabfb21343fb1156de96a01744f7c0972cd5829
Yawar Hussain <yawarhussain793@gmail.com>
2024-05-03 12:51:52 -0700

    Function Intro in java

---
+++ functions_01.java
@@ -0,0 +1,10 @@
+class function_01{
+    public static void print_name()
+    {
+        System.out.print("Hi My name is Yawar Hussain");
+    }
+    public static void main(String args[])
+    {
+        print_name();
+    }
+}
